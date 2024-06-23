831d7c8a90335f76ccc2ef99d25488f43e1b2bec
Nathan Andrade <nathan.nhdsk@gmail.com>
2024-06-23 13:16:52 -0300

    feat: adicionar InputParser para gerar arquivos de input

--- src/br/ufrj/ic/Main.java
+++ src/br/ufrj/ic/Main.java
@@ -1,45 +1,27 @@
 package br.ufrj.ic;
 
 import br.ufrj.ic.matrix.Matrix;
-import br.ufrj.ic.matrix.MatrixImpl;
 import br.ufrj.ic.strassen.SequentialStrassenImpl;
 import br.ufrj.ic.utils.Metrics;
+import br.ufrj.ic.utils.Utils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        // TODO: receber os argumentos do programa.
-
+    public static void main(String[] args) throws IOException {
         if(args.length != 3) {
-            System.out.println("Usage: <input_filename> <output_filename> <threads>");
+            System.out.println("Usage: <matrix1_input_filename> <matrix2_input_filename> <threads>");
             System.exit(1);
         }
 
-        String inputFileName = args[0];
-        String outputFileName = args[1];
+        String firstMatrixInputFileName = args[0];
+        String secondMatrixInputFileName = args[1];
         Integer threads = Integer.parseInt(args[2]);
 
+        Matrix A = Utils.parseInput(firstMatrixInputFileName);
+        Matrix B = Utils.parseInput(secondMatrixInputFileName);
 
         long start = System.nanoTime();
-        List<List<Integer>> matrixDataA = new ArrayList<>();
-
-        matrixDataA.add(Arrays.asList(1, 2, 8));
-        matrixDataA.add(Arrays.asList(2, 4, 5));
-        matrixDataA.add(Arrays.asList(5, 3, 1));
-
-        Matrix A = new MatrixImpl(matrixDataA);
-
-        List<List<Integer>> matrixDataB = new ArrayList<>();
-
-        matrixDataB.add(Arrays.asList(1, 2, 3));
-        matrixDataB.add(Arrays.asList(3, 4, 7));
-        matrixDataB.add(Arrays.asList(1, 9, 4));
-
-        Matrix B = new MatrixImpl(matrixDataB);
-
         Matrix C = SequentialStrassenImpl.execute(A, B);
         long end = System.nanoTime();
 
--- src/br/ufrj/ic/utils/parsers/InputParser.java
+++ src/br/ufrj/ic/utils/parsers/InputParser.java
@@ -1,22 +1,9 @@
 package br.ufrj.ic.utils.parsers;
 
 public class InputParser {
-    private final Integer dimension;
-    private final Integer threads;
-    private final String inputFileName;
-
-    protected String DEFAULT_INPUT_FILE_NAME = "matrix.txt";
-    protected Integer DEFAULT_THREADS_VALUE = 4;
-
-    public InputParser() {
-        this.dimension = (int) Math.pow(10, 4);
-        this.inputFileName = DEFAULT_INPUT_FILE_NAME;
-        this.threads = DEFAULT_THREADS_VALUE;
-    }
-
-    public InputParser(Integer dimension, String inputFileName) {
-        this(dimension, inputFileName, null);
-    }
+    private Integer dimension;
+    private Integer threads;
+    private String inputFileName;
 
     public InputParser(Integer dimension, String inputFileName, Integer threads) {
         this.dimension = dimension;
@@ -27,17 +14,5 @@ public class InputParser {
     public void generateInput() {
         MatrixGenerator matrixGenerator = new MatrixGenerator();
         matrixGenerator.generateMatrix(dimension, threads, inputFileName);
-    }
-
-    public Integer getDimension() {
-        return dimension;
-    }
-
-    public String getInputFileName() {
-        return inputFileName;
-    }
-
-    public Integer getThreads() {
-        return threads;
     }
 }
